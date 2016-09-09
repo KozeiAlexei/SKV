dc8a25172a8b88b94f9a68ca8fb84cc61c017b1b
Kozei Alexei <kav5561123@gmail.com>
2016-09-09 13:52:46 +0300

    FakeCommit

--- SKV/SKV.PL/Controllers/Home/UserManagerController.cs
+++ SKV/SKV.PL/Controllers/Home/UserManagerController.cs
@@ -1,21 +1,9 @@
-﻿using Microsoft.AspNet.Identity.Owin;
-using SKV.BLL.Identity;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace SKV.PL.Controllers.Home
 {
     public class UserManagerController : ControllerExt
     {
-        public IdentityUserManager UserManager
-        {
-            get { return Request.GetOwinContext().GetUserManager<IdentityUserManager>(); }
-        }
-
-        public async Task<ActionResult> Index() => View(await UserManager.GetUsers());
+        public ViewResult Index() => View();
     }
 }
